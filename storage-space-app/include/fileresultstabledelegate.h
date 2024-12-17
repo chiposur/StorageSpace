1ad38b7c96172c3f260c52fdd7a3e95b34b909d4
chiposur <osurov314@gmail.com>
2024-12-16 21:34:24 -0800

    Paint custom mac-style button for FileResultsTable btn cols

--- storage-space-app/include/fileresultstabledelegate.h
+++ storage-space-app/include/fileresultstabledelegate.h
@@ -10,6 +10,18 @@ public:
         QPainter *painter,
         const QStyleOptionViewItem &option,
         const QModelIndex &index) const override;
+
+private:
+    void paintCustomMacButton(
+        QPainter *painter,
+        const QStyleOptionViewItem &option,
+        const QString &text
+    ) const;
+
+    static QRect getRectFromOption(const QStyleOptionViewItem &option, int margin);
+
+    static const QString OPEN_IN_FOLDER_TXT;
+    static const QString DELETE_TXT;
 };
 
 #endif // FILERESULTSTABLEDELEGATE_H
--- storage-space-app/src/fileresultstabledelegate.cpp
+++ storage-space-app/src/fileresultstabledelegate.cpp
@@ -4,8 +4,12 @@
 #include <QApplication>
 #include <QPainter>
 #include <QRect>
+#include <QStyleHints>
 #include <QStyleOptionButton>
 #include <QtSystemDetection>
+
+const QString FileResultsTableDelegate::OPEN_IN_FOLDER_TXT = "Open in folder";
+const QString FileResultsTableDelegate::DELETE_TXT = "Delete";
 
 void FileResultsTableDelegate::paint(
     QPainter *painter,
@@ -26,37 +30,61 @@ void FileResultsTableDelegate::paint(
         QStyledItemDelegate::paint(painter, option, index);
         return;
     }
-    else if (index.column() == 2)
+    else if (index.column() == FileResultsTable::OPEN_IN_FOLDER_COL)
     {
-        btnText = "Open in folder";
+        btnText = OPEN_IN_FOLDER_TXT;
     }
     else
     {
-        btnText = "Delete";
+        btnText = DELETE_TXT;
+    }
+    if (isMac)
+    {
+        // QStyleOptionButton doesn't work correctly for Mac
+        paintCustomMacButton(painter, option, btnText);
+        return;
     }
     QStyleOptionButton btn;
-    int margin = isMac ? 0 : 4;
-    btn.rect =
-        QRect(
-            option.rect.left() + margin,
-            option.rect.top() + margin,
-            option.rect.width() - margin * 2,
-            option.rect.height() - margin * 2);
+    btn.rect = getRectFromOption(option, 4);
     btn.text = btnText;
     btn.state |= QStyle::State_Enabled;
     auto font = painter->font();
-    int pixelSize = isMac ? 12 : 14;
+    int pixelSize = 14;
     font.setPixelSize(pixelSize);
     if (option.state & QStyle::State_MouseOver)
     {
         btn.state |= QStyle::State_MouseOver;
-        if (isMac)
-        {
-            btn.palette.setColor(QPalette::ButtonText, QColor::fromRgb(0, 102, 255));
-        }
     }
     painter->save();
     painter->setFont(font);
     QApplication::style()->drawControl(QStyle::CE_PushButton, &btn, painter);
     painter->restore();
+}
+
+void FileResultsTableDelegate::paintCustomMacButton(
+    QPainter *painter,
+    const QStyleOptionViewItem &option,
+    const QString &text) const
+{
+    painter->save();
+    bool darkMode = QGuiApplication::styleHints()->colorScheme() == Qt::ColorScheme::Dark;
+    auto btnBg = darkMode ? QColor::fromRgb(101, 101, 101) : QColor::fromRgb(236, 236, 236);
+    auto bgRect = getRectFromOption(option, 4);
+    painter->fillRect(bgRect, btnBg);
+    auto font = painter->font();
+    font.setPixelSize(12);
+    painter->setFont(font);
+    painter->setPen(darkMode ? QColor::fromRgb(231, 231, 231) : Qt::black);
+    QTextOption textOption(Qt::AlignmentFlag::AlignVCenter | Qt::AlignHCenter);
+    painter->drawText(QRectF(bgRect), text, textOption);
+    painter->restore();
+}
+
+QRect FileResultsTableDelegate::getRectFromOption(const QStyleOptionViewItem &option, int margin)
+{
+    return QRect(
+        option.rect.left() + margin,
+        option.rect.top() + margin,
+        option.rect.width() - margin * 2,
+        option.rect.height() - margin * 2);
 }
